79fafbe25c7a5abea40d24c4409f8921ad8f509c
Brian Qian <50976186+QianBrian@users.noreply.github.com>
2022-02-28 12:10:37 -0500

    Added comments to code

--- bulk/createLDCT.sh
+++ bulk/createLDCT.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# initiate array for cell types from BED directory
 declare -a files=(~/scratch/ochrodbProject/data/NCLprocessedBED/*)
 
+# for each cell type, echo out path names for the generated outputs from previous steps
 for t in "${files[@]}"; do
     declare noPathName="${t##*/}"
     declare noExt="${noPathName%.bed}"
@@ -10,6 +12,7 @@ for t in "${files[@]}"; do
     # attach the following to the echo command above if using a compiled reference in processing
     #,~/scratch/ochrodbProject/data/NCLcompiledStep2/uniqueCompiledReference.
 
+# save all prints into file for use in step3
 done > all.ldct
 
 echo "done run"
