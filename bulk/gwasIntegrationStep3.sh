b0cd8a15c70a5892cf41b506888a6917f8e68dc3
Brian Qian <50976186+QianBrian@users.noreply.github.com>
2022-02-28 12:08:55 -0500

    Added comments for code

--- bulk/gwasIntegrationStep3.sh
+++ bulk/gwasIntegrationStep3.sh
@@ -3,11 +3,12 @@
 #SBATCH --mem-per-cpu=8000M
 #SBATCH --account=def-pshoosht
 
+# user edited array of GWAS summary statistic files that they intend to integrate with ldsc outputs
 declare -a GWAS=("PASS_Alzheimer.sumstats" "PASS_Anorexia.sumstats" "PASS_Autism.sumstats" "PASS_BMI1.sumstats" "PASS_Bipolar_Disorder.sumstats"
                 "PASS_Celiac.sumstats" "PASS_Coronary_Artery_Disease.sumstats" "PASS_Crohns_Disease.sumstats" "PASS_DS.sumstats"
                 )
 
-
+# perform the GWAs integration for each GWAS sumstats included in the array above
 for t in "${GWAS[@]}"; do
     echo "$t"
     echo "${t%.*}"
