b9c6f9c4801f145c9a450916bb390dd269240687
Brian Qian <50976186+QianBrian@users.noreply.github.com>
2022-02-28 11:33:22 -0500

    Added comments throughout code

--- bulk/makeAnnotationsStep1.sh
+++ bulk/makeAnnotationsStep1.sh
@@ -3,16 +3,26 @@
 #SBATCH --mem-per-cpu=30000M
 #SBATCH --account=def-pshoosht
 
+# initialize int array of 1-22. This is to accomodate chromosome numbers 1 to 22
 vals=($(seq 1 1 22))
 
+# performing these steps for each chromosome number
 for val in ${vals[@]}; do
     echo $val
+    
+    # initialize list containing all different cell types in directory of BED files
     declare -a files=(~/scratch/ochrodbProject/data/NCLprocessedBED/*)
-
+    
+    # iterate through each cell type (aka BED file found in directory) and run `make_annot.py` from ldsc for each one
     for t in "${files[@]}"; do
+        
+        # get path name of file
         declare noPathName="${t##*/}"
+        # get name of file without pathname and file extension
         declare noExt="${noPathName%.bed}"
         echo "$noExt"
+        
+        # run ldsc script
         python2 ~/scratch/ochrodbProject/ldsc/make_annot.py \
                --bed-file $t \
                --bimfile ~/scratch/ochrodbProject/data/hg38/extractedFiles/plink_files/1000G.EUR.hg38.$val.bim \
