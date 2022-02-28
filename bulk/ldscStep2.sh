abd6a3b067e33b7a6abe85e4ee5eab81579421ad
Brian Qian <50976186+QianBrian@users.noreply.github.com>
2022-02-28 12:07:51 -0500

    Added comments for code

--- bulk/ldscStep2.sh
+++ bulk/ldscStep2.sh
@@ -4,17 +4,24 @@
 #SBATCH --account=def-pshoosht
 #SBATCH --array=1-4
 
+# initiate array containing cell type names from BED directory
 declare -a files=(~/scratch/ochrodbProject/data/NCLprocessedBED/*)
 
+# run chromosome numbers for this SLURM job
 val=${SLURM_ARRAY_TASK_ID}
 echo $val
 
+# iterate through cell types array, run ldsc script for each cell type
 for t in "${files[@]}"; do
     echo "$t"
     echo ${SLURM_ARRAY_TASK_ID}
+    
+    # get file names and path names
     declare noPathName="${t##*/}"
     declare noExt="${noPathName%.bed}"
     echo "$noExt"
+    
+    # run ldsc script
     python2 ~/scratch/ochrodbProject/ldsc/ldsc.py \
            --l2 \
            --bfile \
