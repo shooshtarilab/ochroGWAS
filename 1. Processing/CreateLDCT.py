04e93b8b668a7710318b656cc9cae1ba08664f79
Akash Chandra Das <68509886+akashchdas@users.noreply.github.com>
2022-08-05 17:25:20 -0400

    Update CreateLDCT.py

--- 1. Processing/CreateLDCT.py
+++ 1. Processing/CreateLDCT.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import os
-path = 'C:/Users/akash/Desktop/Western University/Outputs/FetalBedFiles'
+path = 'C:/Users/bedfiles'
 files = os.listdir(path)
 names = []
 for i in files:
     names.append(i.split(".")[0])
 for i in names:
-    print("{}\t~/MITACS/FetalLDSCores/{}.".format(i,i))
+    print("{}\t~/Users/bedfiles/{}.".format(i,i))
