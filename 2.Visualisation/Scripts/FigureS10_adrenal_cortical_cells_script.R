c3a25b4d9534c772381551576183aed75ffa3424
Akash Chandra Das <68509886+akashchdas@users.noreply.github.com>
2022-09-13 04:35:18 +0530

    Update FigureS10_adrenal_cortical_cells_script.R

--- 2.Visualisation/Scripts/FigureS10_adrenal_cortical_cells_script.R
+++ 2.Visualisation/Scripts/FigureS10_adrenal_cortical_cells_script.R
@@ -23,7 +23,7 @@ d1 = t(d1)
 d2 = t(d2)
 
 #Output
-png("FigureS10.png", width=4000, height=1750, res=300)
+png("FigureS10.png", res=300, width=2500, height=1500)
 
 #Colour gradient
 col_fun = colorRamp2(c(0, 1), c("#EF3B2C", "white"))
